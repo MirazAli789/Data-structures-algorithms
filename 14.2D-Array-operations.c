8ef17d27bbce6aeb2237de22f0a16fda09229370
Syed Miraz Ali <mirajlegend23@gmail.com>
2023-08-29 22:43:06 +0530

    Update 14.2D-Array-operations.c

--- 14.2D-Array-operations.c
+++ 14.2D-Array-operations.c
@@ -1,151 +1,205 @@
 #include <stdio.h>
-
-void matrix_add(int n, int m, int arr1[10][10], int arr2[10][10])
-{
-    int arr3[10][10];
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            arr3[i][j] = arr1[i][j] + arr2[i][j];
-        }
-    }
-    printf("Matrix after addition\n");
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            printf("%d ", arr3[i][j]);
-        }
-        printf("\n");
-    }
-}
-void matrix_substract(int n, int m, int arr1[10][10], int arr2[10][10])
-{
-    int arr3[10][10];
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            arr3[i][j] = arr1[i][j] - arr2[i][j];
-        }
-    }
-    printf("Matrix after substraction\n");
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            printf("%d ", arr3[i][j]);
-        }
-        printf("\n");
-    }
-}
-void matrix_multiply(int n, int m, int a[10][10], int b[10][10])
+void matrix_add(int arr1[10][10], int arr2[10][10], int n1, int m1, int n2, int m2)
 {
     int c[10][10];
-
-    for (int i = 0; i < n; i++)
+    if (n1 == n2 && m1 == m2)
     {
-        for (int j = 0; j < m; j++)
+
+        for (int i = 0; i < n1; i++)
         {
-            c[i][j] = 0;
-            for (int k = 0; k < m; k++)
+            for (int j = 0; j < m2; j++)
             {
-                c[i][j] += a[i][k] * b[k][j];
+                c[i][j] = arr1[i][j] + arr2[i][j];
             }
         }
+        printf("Matix after addition\n");
+        for (int i = 0; i < n1; i++)
+        {
+            for (int j = 0; j < m2; j++)
+            {
+                printf("%d ", c[i][j]);
+            }
+            printf("\n");
+        }
     }
-    printf("array after multiplication\n");
-    for (int i = 0; i < n; i++)
+    else
     {
-        for (int j = 0; j < m; j++)
+        printf("Addition is not possible\n");
+    }
+}
+void matrix_substract(int arr1[10][10], int arr2[10][10], int n1, int m1, int n2, int m2)
+{
+    int c[10][10];
+    if (n1 == n2 && m1 == m2)
+    {
+
+        for (int i = 0; i < n1; i++)
+        {
+            for (int j = 0; j < m2; j++)
+            {
+                c[i][j] = arr1[i][j] - arr2[i][j];
+            }
+        }
+        printf("Matix after substraction\n");
+        for (int i = 0; i < n1; i++)
+        {
+            for (int j = 0; j < m2; j++)
+            {
+                printf("%d ", c[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        printf("Substraction is not possible\n");
+    }
+}
+void matrix_multiply(int arr1[10][10], int arr2[10][10], int n1, int m1, int n2, int m2)
+{
+    int c[10][10];
+    if (m1 == n2)
+    {
+        for (int i = 0; i < n1; i++)
+        {
+            for (int j = 0; j < m2; j++)
+            {
+                c[i][j] = 0;
+                for (int k = 0; k < m1; k++)
+                {
+                    c[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        printf("Matrix after multiplication\n");
+        for (int i = 0; i < n1; i++)
+        {
+            for (int j = 0; j < m2; j++)
+            {
+                printf("%d ", c[i][j]);
+            }
+            printf("\n");
+        }
+    }
+
+    else
+    {
+        printf("Matrix multiplication is not posiible\n");
+    }
+}
+void matrix_transpoze(int arr1[10][10], int n1, int m1)
+{
+    int c[10][10];
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < m1; j++)
+        {
+            c[j][i] = arr1[i][j];
+        }
+    }
+    printf("Matrix after transpoze\n");
+    for (int i = 0; i < m1; i++)
+    {
+        for (int j = 0; j < n1; j++)
         {
             printf("%d ", c[i][j]);
         }
         printf("\n");
     }
 }
-void matrix_transpoze(int n, int m, int a[10][10], int b[10][10])
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            b[j][i] = a[i][j];
-        }
-    }
-    printf("Array after transpoze\n");
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("%d ", b[i][j]);
-        }
-        printf("\n");
-    }
-}
 int main()
 {
-    int n, m, a[10][10], b[10][10], choice, i = 0, decide;
+    int n1, n2, m1, m2, arr1[10][10], arr2[10][10], i = 0, decide, choice;
     while (i < 1000)
     {
 
-        printf("Enter the number of the rows of the matrices\n");
-        scanf("%d", &n);
-        printf("Enter the number of coloumns of the matrices\n");
-        scanf("%d", &m);
-        printf("Enter the elements of the 1st matrix\n");
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                scanf("%d", &a[i][j]);
-            }
-        }
-        printf("The 1st matrix \n");
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                printf("%d ", a[i][j]);
-            }
-            printf("\n");
-        }
-        printf("Enter the elements of the 2nd matrix\n");
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                scanf("%d", &b[i][j]);
-            }
-        }
-        printf("The 2nd matrix \n");
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                printf("%d ", b[i][j]);
-            }
-            printf("\n");
-        }
-        printf("\nEnter 1 for matrix addition \n Enter 2 for matrix substraction \n Enter 3 for matrix multipliacation\n Enter 4 for matrix transpoze\n");
+        printf("\n Enter 1 for matrix addition \n Enter 2 for matrix substraction \n Enter 3 for matrix multipliacation\n Enter 4 for matrix transpoze\n");
         scanf("%d", &choice);
-        switch (choice)
+        if (choice == 4)
         {
-        case 1:
-            matrix_add(n, m, a, b);
-            break;
-        case 2:
-            matrix_substract(n, m, a, b);
-            break;
-        case 3:
-            matrix_multiply(n, m, a, b);
-            break;
-        case 4:
-            matrix_transpoze(n, m, a, b);
-        default:
-            break;
+            printf("Enter the number of rows of the matrix\n");
+            scanf("%d", &n1);
+            printf("Enter the number of coloumns of the matrix\n");
+            scanf("%d", &m1);
+            printf("Enter the elements of the matrix\n");
+            for (int i = 0; i < n1; i++)
+            {
+                for (int j = 0; j < m1; j++)
+                {
+                    scanf("%d", &arr1[i][j]);
+                }
+            }
+            printf("The first matrix is \n");
+            for (int i = 0; i < n1; i++)
+            {
+                for (int j = 0; j < m1; j++)
+                {
+                    printf("%d ", arr1[i][j]);
+                }
+                printf("\n");
+            }
+            matrix_transpoze(arr1, n1, m1);
         }
+        else
+        {
+            printf("Enter the number of rows of the 1st matrix\n");
+            scanf("%d", &n1);
+            printf("Enter the number of coloumns of the 1st matrix\n");
+            scanf("%d", &m1);
+            printf("Enter the elements of the first matrix\n");
+            for (int i = 0; i < n1; i++)
+            {
+                for (int j = 0; j < m1; j++)
+                {
+                    scanf("%d", &arr1[i][j]);
+                }
+            }
+            printf("The first matrix is \n");
+            for (int i = 0; i < n1; i++)
+            {
+                for (int j = 0; j < m1; j++)
+                {
+                    printf("%d ", arr1[i][j]);
+                }
+                printf("\n");
+            }
+            printf("Enter the number of rows of the 2nd matrix\n");
+            scanf("%d", &n2);
+            printf("Enter the number of coloumns of the 2nd matrx\n");
+            scanf("%d", &m2);
+            printf("Enter the elements of the 2nd matrix\n");
+            for (int i = 0; i < n2; i++)
+            {
+                for (int j = 0; j < m2; j++)
+                {
+                    scanf("%d", &arr2[i][j]);
+                }
+            }
+            printf("The 2nd matrix is\n");
+            for (int i = 0; i < n2; i++)
+            {
+                for (int j = 0; j < m2; j++)
+                {
+                    printf("%d ", arr2[i][j]);
+                }
+                printf("\n");
+            }
+            switch (choice)
+            {
+            case 1:
+                matrix_add(arr1, arr2, n1, m1, n2, m2);
+                break;
+            case 2:
+                matrix_substract(arr1, arr2, n1, m1, n2, m2);
+                break;
+            case 3:
+                matrix_multiply(arr1, arr2, n1, m1, n2, m2);
+                break;
+            default:
+                break;
+            }
+        }
+
         printf("Press 1 if you want to continue, else press any other number to exit\n");
         scanf("%d", &decide);
         if (decide == 1)
@@ -156,8 +210,58 @@ int main()
         {
             break;
         }
-
         i++;
     }
     return 0;
 }
+
+
+ Enter 1 for matrix addition
+ Enter 2 for matrix substraction
+ Enter 3 for matrix multipliacation
+ Enter 4 for matrix transpoze
+3
+Enter the number of rows of the 1st matrix
+3
+Enter the number of coloumns of the 1st matrix
+3
+Enter the elements of the first matrix
+1
+2
+3
+4
+5
+6
+7
+8
+9
+The first matrix is
+1 2 3
+4 5 6
+7 8 9
+Enter the number of rows of the 2nd matrix
+3
+Enter the number of coloumns of the 2nd matrx
+3
+Enter the elements of the 2nd matrix
+1
+2
+3
+4
+5
+6
+7
+8
+9
+The 2nd matrix is
+1 2 3
+4 5 6
+7 8 9
+Matrix after multiplication
+30 36 42
+66 81 96
+102 126 150
+Press 1 if you want to continue, else press any other number to exit
+0
+
+Press any key to continue . . .
