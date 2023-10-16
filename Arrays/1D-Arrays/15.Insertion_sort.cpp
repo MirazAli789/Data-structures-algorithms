dfcb8d0d847b38e294c9b15b4c2a1c28accdc016
Syed Miraz Ali <mirajlegend23@gmail.com>
2023-10-16 08:45:57 +0530

    Create 15.Insertion_sort.cpp

---
+++ Arrays/1D-Arrays/15.Insertion_sort.cpp
@@ -0,0 +1,38 @@
+// for increasing order
+#include <bits/stdc++.h>
+using namespace std;
+void insert_sort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (arr[j] < arr[j - 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+void display(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    int arr[5] = {2, 1, 5, 3, 9};
+    display(arr, 5);
+    insert_sort(arr, 5);
+    display(arr, 5);
+    return 0;
+}
